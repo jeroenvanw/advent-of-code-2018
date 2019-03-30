fd19df3820aaa5e83cdbd3b84593d67a4cfc940b
jeroen.van.wijgerden@philips.com <phnl320044694@MACC02XG4X4JG5M.local>
2019-03-30 19:25:03 +0100

    Add solution to 5, part 2

--- src/advent_of_code_2018/5.clj
+++ src/advent_of_code_2018/5.clj
@@ -2,9 +2,10 @@
   (:require [advent-of-code-2018.input :as input]
             [clojure.string :as string]))
 
+(def a-z (map char (range 97 123)))
+
 (def reaction-patterns
-  (let [a-z (map char (range 97 123))
-        aA-pairs (map #(str % (string/upper-case %)) a-z)]
+  (let [aA-pairs (map #(str % (string/upper-case %)) a-z)]
     (concat aA-pairs (map string/reverse aA-pairs))))
 
 (def regexp (re-pattern (string/join "|" reaction-patterns)))
@@ -15,5 +16,13 @@
     polymer
     (recur post-reaction))))
 
-(let [polymer (first (input/lines 5))]
-  (count (chain-react polymer)))
+(def polymer (first (input/lines 5)))
+
+;; part 1
+(count (chain-react polymer))
+
+;; part 2
+(defn strip [unit]
+  (string/replace polymer (re-pattern (str unit "|" (string/upper-case unit))) ""))
+
+(apply min (map (comp count chain-react strip) a-z))
