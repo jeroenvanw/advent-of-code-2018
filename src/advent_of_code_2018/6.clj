ce400362f1c0a96f86a8bd66abcc0f88f2bb0b19
jeroen.van.wijgerden@philips.com <phnl320044694@MACC02XG4X4JG5M.ddns.htc.nl.philips.com>
2019-04-10 15:50:41 +0200

    Some k-d tree implementation

--- src/advent_of_code_2018/6.clj
+++ src/advent_of_code_2018/6.clj
@@ -1,13 +1,12 @@
 (ns advent-of-code-2018.6
   (:require [advent-of-code-2018.input :as input]
-            [clojure.string :as string]
-            [clojure.core.async :as async]))
+            [clojure.string :as string]))
 
 ;; ([x y] ...)
 (def coordinates
   (let [lines (input/lines 6)
         ->coordinate (fn [line] (vec (map #(Integer/parseInt %) (string/split line #", "))))]
-    (map ->coordinate (take (min 5 (count lines)) lines))))
+    (map ->coordinate (take (min 15 (count lines)) lines))))
 
 (defn x [[x y]] x)
 
@@ -18,6 +17,9 @@
 ;; for each location, find nearest neighbor in k-d tree
 ;; (defn nn "returns nearest neighbor of coordinate in kd-tree" [location] )
 
+(def sorted-by-x (vec (sort-by x coordinates)))
+(def sorted-by-y (vec (sort-by y coordinates)))
+
 (defn make-node [value]
   {:value value :left-child nil :right-child nil})
 
@@ -25,11 +27,8 @@
   (and  (nil? (node :left-child))
         (nil? (node :right-child))))
 
-(defn put-left-child [parent child]
-  (update parent :left-child (fn [_] child)))
+(defn set-left-child [parent child]
+  (update parent :left-child (fn [_] (identity child))))
 
-(defn put-right-child [parent child]
-  (update parent :right-child (fn [_] child)))
-
-(def x:y (sort-by x (sort-by y coordinates)))
-(def y:x (sort-by y (sort-by x coordinates)))
+  (defn set-right-child [parent child]
+    (update parent :right-child (fn [_] (identity child))))
